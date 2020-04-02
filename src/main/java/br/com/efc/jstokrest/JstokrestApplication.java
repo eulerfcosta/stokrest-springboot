54067ea1a612164549af06e62577a3eeb4456b43
eulerfcosta <euler.dacosta@gmail.com>
2020-04-01 23:42:19 -0300

    c001 - Commiti inical do projeto

---
+++ src/main/java/br/com/efc/jstokrest/JstokrestApplication.java
@@ -0,0 +1,13 @@
+package br.com.efc.jstokrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JstokrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JstokrestApplication.class, args);
+	}
+
+}
